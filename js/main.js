b09582d103229184f19e3017968efcf20ca57b28
sidhbk08 <92094261+sidhbk08@users.noreply.github.com>
2025-07-08 07:32:59 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@
 		tempStar: null,
 		choose: [],
 		level: parseInt(localStorage.getItem("star_match_level") || "1"),
+		localStorage.setItem("star_match_level", computed.level);
 		score: 0,	
 		totalScore: 0,
 		stepTargetScore: 500,	
@@ -379,8 +380,8 @@ new CrushGame();
 					}
 					star.row = i;
 					star.col = j; 
-					star.style.left = Math.floor(starSet[i][j].col * config.starWidth * 53.333333) + "px";
-					star.style.bottom = Math.floor(starSet[i][j].row * config.starHeight * 53.333333) + "px";
+					star.style.left = (starSet[i][j].col * config.starWidth) + "rem";
+					star.style.bottom = (starSet[i][j].row * config.starHeight) + "rem";
 					star.style.backgroundImage = "url('images/" + starSet[i][j].number + ".png')";
 				}
 			}
